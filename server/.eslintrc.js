03ab8792f6314831c3071cc30a90845617dda025
seonghyeon <hyeon@apartmentary.com>
2018-10-09 11:29:06 +0900

    server - update eslint configuration

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -2,5 +2,12 @@ module.exports = {
   "extends": "airbnb-base",
   "env": {
     "node": true,
+    "mocha": true,
+  },
+  "rules": {
+    "import/no-extraneous-dependencies": [
+      "error",
+      {"devDependencies": ["**/test/**/*.js"]},
+    ],
   },
 };
