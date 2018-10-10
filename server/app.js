3199c81828eca9473541deead257e9cf65feb5db
seonghyeon <hyeon@apartmentary.com>
2018-10-11 01:47:06 +0900

    server - add calendar collection reset

--- server/app.js
+++ server/app.js
@@ -5,25 +5,34 @@ const bodyParser = require('body-parser');
 const CONFIG = require('config');
 
 const calendar = require('./routes/calendar');
+const models = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 const { mongodb } = CONFIG;
+const { Calendar } = models;
 
-// mongodb
-mongoose.Promise = Promise;
-if (mongoose.connection.readyState === 0) {
-  mongoose.connect(`mongodb://${mongodb.host}:${mongodb.port}/${mongodb.database}`, { useNewUrlParser: true });
-}
+(async () => {
+  // mongodb
+  mongoose.Promise = Promise;
+  if (mongoose.connection.readyState === 0) {
+    mongoose.connect(
+      `mongodb://${mongodb.host}:${mongodb.port}/${mongodb.database}`,
+      { useNewUrlParser: true },
+    );
+  }
+  // reset calendar collectoin
+  await Calendar.deleteMany();
 
-// express
-app.use(bodyParser.json());
-app.use('/api/calendar', calendar);
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
-    // eslint-disable-next-line
-    console.log(`server listening on port ${PORT}`);
-  });
-}
+  // express
+  app.use(bodyParser.json());
+  app.use('/api/calendar', calendar);
+  if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => {
+      // eslint-disable-next-line
+      console.log(`server listening on port ${PORT}`);
+    });
+  }
+})();
 
 module.exports = app;
