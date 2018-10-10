90b87ef1ab291a2aeca6c083b0c7119b21627e76
seonghyeon <hyeon@apartmentary.com>
2018-10-10 22:43:06 +0900

    server - add POST /api/calendar

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -1,5 +1,38 @@
 const express = require('express');
+const errors = require('../errors');
+const { Calendar } = require('../models');
+const { sendError } = require('../utils/routeUtils');
+
 const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const { title, start_date, end_date } = req.body;
+  if (!title || !start_date || !end_date) {
+    sendError(res);
+    return;
+  }
+
+  const condition = {
+    start_date: {
+      $gte: start_date,
+      $lt: end_date,
+    },
+  };
+  const calendar = await Calendar.findOne(condition).lean().exec();
+  if (calendar) {
+    sendError(res, errors.calendar_duplicate_data);
+    return;
+  }
+
+  const postData = {
+    title,
+    start_date,
+    end_date,
+  };
+
+  await Calendar.create(postData);
+  return res.json({});
+});
 
 router.get('/', (req, res) => {
   res.send('Hello World');
--- server/test/routes/calendar.test.js
+++ server/test/routes/calendar.test.js
@@ -1,9 +1,56 @@
 const chai = require('chai');
+const moment = require('moment');
 
-const { app } = chai;
+const {
+  app,
+  assertError,
+  errors,
+  models,
+} = chai;
+const { route_invalid_data, calendar_duplicate_data } = errors;
+const { Calendar } = models;
+const should = chai.should();
 
 
 describe('Calendar Router', () => {
+  after(async () => {
+    await Calendar.deleteMany();
+    const result = await Calendar.findOne();
+    should.equal(result, null);
+  });
+
+  describe('POST /api/calendar', () => {
+    const start_date = moment().toISOString();
+    const end_date = moment().add(1, 'days').toISOString();
+    const postData = {
+      title: 'title',
+      start_date,
+      end_date,
+    };
+
+    it('should return error when data is not enough', async () => {
+      const res = await chai.request(app).post('/api/calendar');
+      res.status.should.be.equal(500);
+      assertError(res.error.text, route_invalid_data);
+    });
+
+    it('should create calendar events successfully', async () => {
+      const res = await chai.request(app)
+        .post('/api/calendar')
+        .send(postData);
+      res.status.should.be.equal(200);
+      res.body.should.be.empty;
+    });
+
+    it('should return error when trying to create calendar events on the same slot', async () => {
+      const res = await chai.request(app)
+        .post('/api/calendar')
+        .send(postData);
+      res.status.should.be.equal(500);
+      assertError(res.error.text, calendar_duplicate_data);
+    });
+  });
+
   it('GET /api/calendar', async () => {
     const res = await chai.request(app).get('/api/calendar');
     res.status.should.be.equal(200);
